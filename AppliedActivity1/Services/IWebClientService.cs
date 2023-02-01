a61e6b3a46a1825cf6e7da36a7b9276257d86b5f
KaranPatel19 <41769712+KaranPatel19@users.noreply.github.com>
2023-01-31 21:08:57 -0500

    Created Web Client Service and Interface

---
+++ AppliedActivity1/Services/IWebClientService.cs
@@ -0,0 +1,13 @@
+﻿ namespace AppliedActivity1.Services
+{
+    public class IWebClientService
+    {
+        public IWebClientService()
+        {
+            Task<string> GetAsync(string uri);
+            Task<string> PostAsync(string uri, string body, string type);
+            Task<string> PostAsync(string uri, string body, string type);
+        }
+    }
+}
+
---
+++ AppliedActivity1/Services/WebClientService.cs
@@ -0,0 +1,31 @@
+﻿
+namespace AppliedActivity1.Services
+{
+	public class WebClientService : IWebClientService
+	{
+        public async Task<string> GetAsync(string uri)
+        {
+            try
+            {
+                HttpClient client;
+                client = new HttpClient();
+
+                HttpResponseMessage response = await client.GetAsync(uri);
+                return response.IsSuccessStatusCode ? await response.Content.ReadAsStringAsync() : null;
+            }
+            catch (Exception ex)
+            {
+                return null;12232
+            }
+        }
+        public async Task<string> PostAsync(string uri, string body, string type)
+        {
+            throw new NotImplementedException();
+        }
+        public async Task<string> PutAsync(string uri, string body, string type)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
